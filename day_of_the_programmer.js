210e5d416c966cb7c7ff48556aa23bc6e9a5e77d
antmortra <antoniomorillotrapero@gmail.com>
2024-05-31 16:26:55 -0400

    hago cambios y la solucion es correcta

--- day_of_the_programmer.js
+++ day_of_the_programmer.js
@@ -10,41 +10,45 @@ const transicion = [31, 15, 31, 30, 31, 30, 31, 31];
 let result;
 
 let day = juliano.reduce((total, value) => total + value);
-// console.log(day);
 
 let dayProg = 256 - day;
-// console.log(dayProg);
+
 let day2 = julianobis.reduce((total, value) => total + value);
-// console.log(day2);
 
 let dayProg2 = 256 - day2;
-// console.log(dayProg2);
 
 let dayTr = transicion.reduce((total, value) => total + value);
-// console.log(dayTr);
 
 let dayProgTr = 256 - dayTr;
-// console.log(dayProgTr);
 
-if ( year < 2018 && year % 4 != 0 ) {
 
-    result = '13.09.' + year;
+if ( year < 1918 && year % 4 != 0 ) {
+
+    result = dayProg + '.09.' + year;
 }
-else if ( year < 2018 && year % 4 == 0 ) {
+else if ( year < 1918 && year % 4 == 0 ) {
 
-    result = '12.09.' + year;
+    result = dayProg2 + '.09.' + year;
 }
-else if ( year == 2018 ) {
+else if ( year == 1918 ) {
 
-    result = '26.09.' + year;
+    result = dayProgTr + '.09.' + year;
 }
-else if ( year > 2018 && year % 4 != 0 ) {
+else if ( year > 1918 && year % 4 != 0 ) {
 
-    result = '13.09.' + year;
+    result = dayProg + '.09.' + year;
 }
-else if ( year > 2018 && year % 4 == 0 && year % 400 == 0 && year % 100 != 0 ) {
+else if ( year > 1918 && year % 400 == 0 ) {
 
-    result = '12.09.' + year;
+    result = dayProg2 + '.09.' + year;
+}
+else if ( year > 1918 && year % 4 == 0 && year % 100 != 0 ) {
+
+    result = dayProg2 + '.09.' + year;
+}
+else {
+
+    result = dayProg + '.09.' + year;
 }
 
-console.log (result);
+return result;
