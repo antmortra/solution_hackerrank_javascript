23495ecd155115bd86d1b8a941157ff99f5ca4dc
antmortra <antoniomorillotrapero@gmail.com>
2024-06-01 11:37:31 -0400

    cambio la variable b ya que la tenia yo para hacer pruebas pero sirve para el test

--- electronic_shop.js
+++ electronic_shop.js
@@ -1,7 +1,7 @@
-let b = 60;
+let b = 10;
 
-const keyboard = [40, 50, 60];
-const drives = [5, 8, 12];
+const keyboard = [3, 1];
+const drives = [5, 2, 8];
 
 const buy = [];
 
@@ -13,7 +13,7 @@ keyboard.forEach( (el) => {
     drives.forEach( (el2) => {
         // console.log(el2)
 
-        if ( el + el2 < 60 ) {
+        if ( el + el2 < b ) {
 
             tempt.push(el+el2);
         }
@@ -24,4 +24,6 @@ keyboard.forEach( (el) => {
 
 const solution = buy.flat();
 // console.log(solution);
-return Math.max.apply(null, solution);
+// return Math.max.apply(null, solution);
+
+console.log(Math.max.apply(null, solution))
