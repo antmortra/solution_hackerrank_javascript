6d3c2690f1e183e1167fb2c8d9e362d714851a63
antmortra <antoniomorillotrapero@gmail.com>
2024-06-01 11:16:25 -0400

    inicio el algoritmo

---
+++ electronic_shop.js
@@ -0,0 +1,18 @@
+let b = 60;
+
+const keyboard = [40, 50, 60];
+const drives = [5, 8, 12];
+
+const buy = [];
+
+keyboard.forEach( el => {
+    console.log(el);
+
+    const tempt = [];
+
+    drives.forEach( el2 => {
+        console.log(el2)
+
+
+    })
+});
